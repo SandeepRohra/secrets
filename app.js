1a1b487898c7d5a51bf787ef74286a411057d194
Sandeep Rohra <sandeep12rohra@gmail.com>
2021-12-22 16:25:12 +0530

    encryption started done first level

---
+++ app.js
@@ -0,0 +1,134 @@
+
+//jshint esversion:6
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+const mongooseEncryption = require('mongoose-encryption');
+const app =express()
+app.use(express.static(`public`));
+app.set(`view engine`,`ejs`);
+app.use(bodyParser.urlencoded({extended:true}))
+mongoose.connect("mongodb://localhost:27017/userDB")
+const userSchema =new mongoose.Schema({
+  email:String,
+  password:String
+});
+var secret = `Thisismyownsecret`;
+userSchema.plugin(mongooseEncryption, { secret: secret,encryptedFields: ['password']})
+
+
+const User =new mongoose.model(`User`,userSchema)
+
+
+
+
+
+
+
+
+app.route(`/`)
+.get(function(req,res){
+  res.render(`home`)
+});
+
+
+
+
+
+
+
+
+
+
+app.route(`/login`)
+.get(function(req,res){
+  res.render(`login`)
+})
+.post(function(req,res){
+const email = req.body.username
+const password =req.body.password
+
+  User.findOne({email:email},function(err,foundUser){
+    if(err){
+      console.log(err);
+    }
+  else{
+    if (foundUser && foundUser.password=== password){
+      console.log(foundUser);
+        res.render(`secrets`)
+      }
+      else{
+          console.log(email,password);
+        res.send(`Invalid email or password`)
+
+      }
+  }
+  })
+});
+
+
+
+
+
+
+
+app.route(`/register`)
+.get(function(req,res){
+  res.render(`register`)
+})
+.post(function(req,res){
+  const newUser=new User({
+    email:req.body.username,
+    password:req.body.password
+  });
+  newUser.save(function(err){
+  if(err){
+    console.log(err);
+  }else{
+    res.render(`secrets`)
+  }
+  })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(3000,function(){
+  console.log(`Server started at port 3000`);
+})
